0b5465b312b2f90507c468deb0b68e4c6c5c2c76
Unerasable <shayanariz99@gmail.com>
2021-12-02 19:32:06 +0530

    📺added the gui

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ from tkinter import *
 # import messagebox from tkinter module
 import tkinter.messagebox
 
-# create a tkinter root window
-
-
-
-
+#creating tkinter root window
 # Initializing the wmi constructor
 f = wmi.WMI()
 def run():
@@ -48,12 +44,25 @@ def run():
             print("Application is not Running")
         root.destroy()
         root.mainloop()
+ 
+def open_file():
+    program_name=program_var.get()
+    with open("program.name", "w") as file:
+        file.write(program_name+'.exe')
+        file.close()
+    win_startup.destroy()
+    run()
 if os.path.exists("program.name") == True:
     run()
+
 else:
-    program = input("The name of the program you would like to block: ")
-    with open("program.name", "w") as file:
-        file.write(program)
-        file.close()
-    run()
+    global win_startup
+    win_startup=tkinter.Tk()
+    win_startup.geometry("400x250")
+    program_var = tkinter.StringVar()
+    program_instruction=Label(win_startup, text="Name of the program that you'd like to block:").grid(row=0, column=0)
+    program = Entry(win_startup,textvariable=program_var).grid(row=0, column=1)
+    sub_btn=Button(win_startup, text='Go!', command=open_file).grid(row = 4, column = 0)
+    win_startup.mainloop()
+
 
