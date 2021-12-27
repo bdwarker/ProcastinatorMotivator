5a5bcc2a1bd3f272cdc0a214fb10b69bf2bb1e95
Unerasable <shayanariz99@gmail.com>
2021-12-27 22:38:41 +0530

    added linux and macos support

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 # Import module
 import os
-os.system('pip install wmi requests')
-import wmi
+os.system('pip install psutil requests')
+import psutil
 import json
 import requests
 import os.path
 import getpass
-
 # import everything from tkinter module
 from tkinter import *
 
 # import messagebox from tkinter module
 import tkinter.messagebox
 import shutil
-
-
-
-#creating tkinter root window
+# creating tkinter root window
 # Initializing the wmi constructor
-f = wmi.WMI()
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
 def run():
     with open("program.name","r") as f_read:
         program_read = f_read.read()
@@ -30,24 +38,20 @@ def run():
 
 
         def get_Quote():
+            #getting the quote
             response = requests.get("https://zenquotes.io/api/random")
             json_data = json.loads(response.text)
             quote = json_data[0]['q'] + " -" + json_data[0]['a']
             return (quote)
-
-
-        # Iterating through all the running processes
-        for process in f.Win32_Process():
-            if program_read == process.Name:
-                print("Running")
-                tkinter.messagebox.showinfo("ProcrastinatorMotivator",
+        # Check if any chrome process was running or not.
+        if checkIfProcessRunning(program_read):
+            tkinter.messagebox.showinfo("ProcrastinatorMotivator",
                                             f"While you are procastinating someone else is working hard to obtain your postion. {get_Quote()}")
-                flag = 1
-                break
 
-        if flag == 0:
-            print("NO")
-            run()
+        else:
+            print('')
+
+        
         root.destroy()
         root.mainloop()
 def createStartup():
